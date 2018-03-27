416b784ef5ec7a42c4386106fd6178dc9e765b84
katerina_makri <kathrin023@gmail.com>
2018-03-27 18:01:15 +0300

    feat: add code of modal
    At CSS: Improvements at UI of modal
    At JS: Create separate functions and add modal
    At HTML: Add basic code for modal

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
-/*
- * Create a list that holds all of your cards
- */
 
-const icons = ["fa-anchor","fa-anchor", "fa-paper-plane-o","fa-paper-plane-o", "fa-diamond","fa-diamond", "fa-cube","fa-cube", "fa-leaf","fa-leaf", "fa-bomb", "fa-bomb", "fa-bicycle", "fa-bicycle", "fa-bolt", "fa-bolt"]
+const icons = [
+	"fa-anchor",
+	"fa-anchor",
+	"fa-paper-plane-o",
+	"fa-paper-plane-o",
+	"fa-diamond",
+	"fa-diamond",
+	"fa-cube",
+	"fa-cube",
+	"fa-leaf",
+	"fa-leaf",
+	"fa-bomb",
+	"fa-bomb",
+	"fa-bicycle",
+	"fa-bicycle",
+	"fa-bolt",
+	"fa-bolt"
+];
+
+//starting the game
+function initialiseGameBoard() {
+	
+	const cards = generateCardTable(icons)
+	initialiseClickActions()
+}
 
 //reset board game
 function generateCardTable(icons) {
@@ -20,7 +41,6 @@ function generateCardTable(icons) {
 	return uiCards
 }
 
-
 //restart button
 const restart = document.getElementById('restartbutton');
 
@@ -34,8 +54,6 @@ function startAgain() {
 	//reset timer
 	clearInterval(timer);
 	timer = null
-
-	// console.log('it works!')
 	document.getElementById('timer').innerHTML = '00:00';
 
 	
@@ -45,6 +63,7 @@ function startAgain() {
 
 	//reset move counter
 	resetCounter();
+	document.getElementById('counter').innerHTML = '0 Moves';
 }
 
 //replay button
@@ -53,7 +72,7 @@ const replay = document.getElementById('replaybutton');
 replay.addEventListener('click', function() {
 	
 	// close modal
-	overlay();
+	toggleOverlay();
 
 	startAgain()
 
@@ -80,45 +99,43 @@ function timerStarts(){
 	if(timer) return;
 
 	let start = Date.now();
-    timer = setInterval(function(){
+		timer = setInterval(function(){
 
-    	sec = Math.round((Date.now() - start) / 1000) % 60;
+			sec = Math.round((Date.now() - start) / 1000) % 60;
 		min = Math.round((Date.now() - start) / 1000) / 60;
 		min = Math.floor(min)
 
 
-    	if (sec.toString().length == 1 && (min.toString().length == 1)){
-    		document.getElementById('timer').innerHTML = '0' + min + ':0' + sec;
-    	}
-    	
-    	if (sec.toString().length == 2 && (min.toString().length == 1)){
+			if (sec.toString().length == 1 && (min.toString().length == 1)){
+				document.getElementById('timer').innerHTML = '0' + min + ':0' + sec;
+			}
+			
+			if (sec.toString().length == 2 && (min.toString().length == 1)){
 
-    		document.getElementById('timer').innerHTML = '0' + min + ':' + sec;
-    	}
-    	if (sec.toString().length == 1 && (min.toString().length == 2) ){
+				document.getElementById('timer').innerHTML = '0' + min + ':' + sec;
+			}
+			if (sec.toString().length == 1 && (min.toString().length == 2) ){
 
-    		document.getElementById('timer').innerHTML = min + ':0' + sec;
-    	}   
-    	
-    	if (sec.toString().length == 2 && (min.toString().length == 2) ){
+				document.getElementById('timer').innerHTML = min + ':0' + sec;
+			}   
+			
+			if (sec.toString().length == 2 && (min.toString().length == 2) ){
 
-    		document.getElementById('timer').innerHTML = min + ':' + sec;
-    	} 
+				document.getElementById('timer').innerHTML = min + ':' + sec;
+			} 
 
-    	var result = "";
-        //stops when all cards are matched
-        if ($(".card.match").length === 16) {
-            clearInterval(timer);
-        }
+				//stops when all cards are matched
+				if ($(".card.match").length === 16) {
+						clearInterval(timer);
+				}
 
-    }, 1000);
-
+		}, 1000);
 }
 
 //calculate stars
 let starsNum;
 
-function stars(movecount){
+function calculateStars(movecount){
 
 	if (movecount < 20){
 		starsNum = 3
@@ -127,31 +144,18 @@ function stars(movecount){
 	if (movecount >= 20 && movecount < 38) {
 		document.getElementById("No3").style.display = "none";
 		starsNum = 2
-		// console.log('it works!')
-		// $('.score-panel .stars li')[2].css("display","none")
 
 	} 
 	if (movecount >= 38) {
 		document.getElementById("No3").style.display = "none";
 		document.getElementById("No2").style.display = "none";
 		starsNum = 1
-		// $('.score-panel .stars li')[1].css("display","none")
 	} 
 }
 
-//starting the game
-function initialiseGameBoard() {
-	
-	const cards = generateCardTable(icons)
-	initialiseClickActions()
-}
-
-initialiseGameBoard()
-
 //function for modal
-function overlay() {
+function toggleOverlay() {
 	const modal = document.getElementById("overlay");
-
 
 	if (modal.style.visibility == "visible") {		
 		
@@ -165,12 +169,44 @@ function overlay() {
 	}
 }
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+function flipCard(card) {
+	if(card.classList.contains("open", "show")) {
+		card.classList.remove('notmatch', 'open','show');
+	}
+	else {
+		card.classList.add('open','show');
+	}
+}
+
+function increaseMoveCounter(){
+	movecount = movecount + 1;
+	if (movecount == 1) {
+		counter.innerHTML = movecount + ' Move'
+	} else {
+		counter.innerHTML = movecount + ' Moves'
+	}
+}
+
+function finishGame(){
+	if (starsNum == 3){
+		document.getElementById('overlay').querySelector("h3").textContent = "Excellent"
+		document.getElementById('overlay').querySelector("p").textContent = "Your time was " + min + ":" + sec +" and you won " + starsNum + " Stars"
+		
+	
+	} else if (starsNum == 2){
+		document.getElementById('overlay').querySelector("h3").textContent = "Well done"
+		document.getElementById('overlay').querySelector("p").textContent = "Your time was " + min + ":" + sec +" and you won " + starsNum + " Stars"
+	
+	} else if (starsNum == 1){
+	
+		document.getElementById('overlay').querySelector("h3").textContent = "Nice try"
+		document.getElementById('overlay').querySelector("p").textContent = "Your time was " + min + ":" + sec +" and you won " + starsNum + " Star"
+		document.getElementById('overlay').querySelector("button").textContent = "Try again"
+
+	}
+	
+	toggleOverlay()
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -188,122 +224,79 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 let movecount = 0;
+
+initialiseGameBoard()
 
 function initialiseClickActions() {
 	const deck = document.getElementById('game-board');
 
 	deck.addEventListener('click', function(evt) {
-
+		
+		// check that the target clicked is indeed a card. Could be the deck or some empty space
 		if(evt.target.tagName === "LI"){
 			const currentCard = evt.target;
-			var notCurrentCard;
+			let anotherCard;
+			let isCardAlreadyOpen = currentCard.classList.contains('open', 'show');
 
 			//timer starts
 			if (document.getElementById('timer').innerHTML =='00:00'){
 				timerStarts();
 			}
 
-			//checks if the currentCard is open
-			if (currentCard.classList.contains('open', 'show')){
-				   
-				//increase move count
-				movecount = movecount + 1;
-				counter.textContent = movecount;
-				
-				//close card
-				currentCard.classList.remove('open','show');
+			flipCard(currentCard);
 
-				//calculate stars
-				stars(movecount);				
-				
-			} else {
+			//increase move count
+			increaseMoveCounter();
+			//calculate stars
+			calculateStars(movecount);
 
-				//open card
-				currentCard.classList.add('open', 'show');  
-
-				//increace count
-				movecount = movecount + 1;
-				counter.textContent = movecount;
-
-				//calculate stars
-				stars(movecount);
+			//checks if the currentCard is already open
+			if(isCardAlreadyOpen){
+				return;
 			}
 
 			//checks if there is 2nd card
 			if($(".card.open.show").length === 2){
 
 				//checks if first or current card is open
-				if ( $(".card.open.show")[0] === currentCard ||  $(".card.open.show")[1] === currentCard ){
+				if ( $(".card.open.show").has(currentCard) ){
 
-					//define notCurrentCard
+					//define anotherCard
 					if ($(".card.open.show")[0] === currentCard){
-					   notCurrentCard = $(".card.open.show")[1]
+						 anotherCard = $(".card.open.show")[1]
 					}
-					if ($(".card.open.show")[1] === currentCard){
-					   notCurrentCard = $(".card.open.show")[0]
+					else {
+						 anotherCard = $(".card.open.show")[0]
+					}
+	
+					// have cards the same icon? (matched)
+					if (anotherCard.querySelector("i").className === currentCard.querySelector("i").className) {
+						anotherCard.classList.add('match')
+						currentCard.classList.add('match')
+						flipCard(anotherCard)
+						flipCard(currentCard)
+
+						// if all cards are matched
+						if ($(".card.match").length === 16){
+							finishGame()
+						}
+
+					}
+					//add notmatch class
+					else {
+						anotherCard.classList.add('notmatch')
+						currentCard.classList.add('notmatch')
+
 					}
 					
-					// check if notCurrentCard is open and not
-					if(notCurrentCard ) {    	
-
-						// have cards the same icon? (matched)
-						if (notCurrentCard.querySelector("i").className === currentCard.querySelector("i").className) {
-							// notCurrentCard.classList.remove('open','show')
-							notCurrentCard.classList.add('match')
-							// currentCard.classList.remove('open','show')
-							currentCard.classList.add('match')
-						}
-
-						//add notmatch class				
-						if (!notCurrentCard.classList.contains('match') && !currentCard.classList.contains('match')){
-							notCurrentCard.classList.add('notmatch')
-							currentCard.classList.add('notmatch')
-						}
-
-						//close both open cards
-						setTimeout(function () {
-							notCurrentCard.classList.remove('notmatch','open','show')
-							currentCard.classList.remove('notmatch','open','show') 
-						}, 500);
-					}
-
-					// console.log(notCurrentCard)
+					//close both open cards
+					setTimeout(function () {
+						anotherCard.classList.remove('notmatch','open','show')
+						currentCard.classList.remove('notmatch','open','show') 
+					}, 500);
 				}
-
 			} 
-
-			// if all cards are matched
-			if ($(".card.match").length === 16){
-				if (starsNum == 3){
-					document.getElementById('overlay').querySelector("h3").innerHTML = "Excellent"
-					document.getElementById('overlay').querySelector("p").innerHTML = "Your time was " + min + ":" + sec +" and you won " + starsNum + " Stars"
-					
-				
-				} else if (starsNum == 2){
-					document.getElementById('overlay').querySelector("h3").innerHTML = "Well done"
-					document.getElementById('overlay').querySelector("p").innerHTML = "Your time was " + min + ":" + sec +" and you won " + starsNum + " Stars"
-				
-				} else if (starsNum == 1){
-				
-					document.getElementById('overlay').querySelector("h3").innerHTML = "Nice try"
-					document.getElementById('overlay').querySelector("p").innerHTML = "Your time was " + min + ":" + sec +" and you won " + starsNum + " Star"
-					document.getElementById('overlay').querySelector("button").innerHTML = "Try again"
-
-				}
-				
-				overlay()
-			}
 
 		}
 
@@ -311,4 +304,3 @@ function initialiseClickActions() {
 
 	});
 }
-
